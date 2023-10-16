f6f89e351715e3cbd88cd8702eb088287a4083be
NatalijaKatic <45798044+NatalijaKatic@users.noreply.github.com>
2023-10-16 21:03:35 +0200

    Add files via upload

---
+++ SAI/notchFilter.m
@@ -0,0 +1,14 @@
+function filtSig = notchFilter(sig,freq)
+Fs=30000;
+q = 35;
+for frequency = freq
+    fo = frequency;
+    bw = (fo/(Fs/2))/q;
+    [b,a] = iirnotch(frequency*2/Fs,bw);
+%     [b,a]=butter(5, [fo-1 fo+1]./(Fs/2), 'stop');
+    sig=filtfilt(b,a,double(sig));
+end
+
+filtSig = sig;
+end
+
